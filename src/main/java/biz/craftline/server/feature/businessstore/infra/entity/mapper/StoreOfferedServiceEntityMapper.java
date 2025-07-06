f77737a84c57dfce2d91c019f879be6c4d5420ca
shiva <shiva.dharmoj@gmail.com>
2025-07-06 12:04:38 +0530

    first commit

--- src/main/java/biz/craftline/server/feature/businessstore/infra/entity/mapper/StoreOfferedServiceEntityMapper.java
+++ src/main/java/biz/craftline/server/feature/businessstore/infra/entity/mapper/StoreOfferedServiceEntityMapper.java
@@ -3,6 +3,8 @@ package biz.craftline.server.feature.businessstore.infra.entity.mapper;
 import biz.craftline.server.feature.businessstore.api.mapper.StoreServicePriceDTOMapper;
 import biz.craftline.server.feature.businessstore.domain.model.StoreOfferedService;
 import biz.craftline.server.feature.businessstore.infra.entity.StoreOfferedServiceEntity;
+import biz.craftline.server.feature.businesstype.api.mapper.BusinessServiceDTOMapper;
+import biz.craftline.server.feature.businesstype.infra.entity.mapper.BusinessServiceEntityMapper;
 import org.mapstruct.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,11 +14,14 @@ public class StoreOfferedServiceEntityMapper {
 
     @Autowired
     StoreServicePriceEntityMapper entityMapper;
+    @Autowired
+    BusinessServiceEntityMapper businessServiceEntityMapper;
 
     public StoreOfferedService toDomain(StoreOfferedServiceEntity dto){
         return StoreOfferedService.builder()
                 .id(dto.getId())
                 .storeId(dto.getStoreId())
+                .service( businessServiceEntityMapper.toDomain(dto.getService()) )
                 .aliasName( dto.getAliasName())
                 .price(entityMapper.toDomain(dto.getPrice()))
                 .build();
